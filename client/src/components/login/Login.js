d986428d8cdc211aae1fdaf1058d07cada24bb9d
Max <maxanderberg@gmail.com>
2021-07-01 10:24:52 +0200

    login form now functioning

--- client/src/components/login/Login.js
+++ client/src/components/login/Login.js
@@ -3,16 +3,22 @@ import {useState} from 'react';
 const Login = () => {
     const [userName, setUser] = useState('');
 
-    const handleSubmit = () => {
+    const handleOnChange = (e) => setUser(e.target.value)
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(userName){
+            console.log(userName)
+        }
+        setUser('');
     }
     return (
         <div className="login">
         <h1 className="login__title">Letter</h1>
-           <form>
+           <form onSubmit={handleSubmit}>
                <h3>Login</h3>
-                <input type="text" name="" value={userName} id="" placeholder="Username..."/>
-                <input type="button" value="Login" />
+                <input type="text" name="" value={userName} id="" placeholder="Username..." onChange={(e) => handleOnChange(e)}/>
+                <input type="submit" value="Login" />
            </form>
         </div>
     )
