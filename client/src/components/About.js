a1b9692fc40853051057b0988840e4899b6b7744
Max <maxanderberg@gmail.com>
2021-07-02 10:05:37 +0200

    About page styled

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,9 +1,11 @@
 import './Main.scss';
+import './About.scss'
 
 const About = (props) => {
     return (
         <div className="main-content-container">
             <section className="about-container">
+                <h1>About Letter</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                 Debitis in doloribus fugit aliquam nihil tempora ullam quam dolorem? 
                 Ab tempore unde non impedit error. 
